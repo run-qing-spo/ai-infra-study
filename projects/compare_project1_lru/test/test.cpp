e7197b5940b9a7a3a27ae424145b8d45fbe73af0
run-qing-spo <13219070380@163.com>
2026-06-14 13:32:32 +0000

    feat(compare_gtest)

--- projects/compare_project1_lru/test/test.cpp
+++ projects/compare_project1_lru/test/test.cpp
@@ -1,33 +1,22 @@
-#include<iostream>
-#include"lru_base.hpp"
-#include"util.hpp"
-#include<cassert>
-#include<vector>
+#include "lru_base.hpp"
 
-int testOne() {
-    std::vector<int> a{1, 2, 4, 3};
-    lru_base::lrucache_base* node = new lru_base::lrucache_base(1);
-    for (auto u:a) {
-        node->push(u);
-    }
-    std::vector<int> b;
-    for (lru_base::lrucache_base* cur = node; cur != nullptr; cur = cur->next) {
-        b.emplace_back(cur->val);
+#include <gtest/gtest.h>
+
+#include <vector>
+
+TEST(LruCacheBaseTest, PushAddsNodesAfterHead) {
+    std::vector<int> values{1, 2, 4, 3};
+    lru_base::lrucache_base node(1);
+
+    for (int value : values) {
+        node.push(value);
     }
 
-    assert(equal_vector(std::vector<int> {1, 3, 4, 2, 1}, b));
-    if (equal_vector(std::vector<int> {1, 3, 4, 2, 1}, b)) {
-        std::cout << " [PASS] testOne" << std::endl;
-    }else {
-        std::cout << " [FAILED] testOne" << std::endl;
-        return 1;
+    std::vector<int> actual;
+    for (lru_base::lrucache_base* cur = &node; cur != nullptr; cur = cur->next) {
+        actual.emplace_back(cur->val);
     }
-    return 0;
-}
 
-int main() {
-
-    return testOne();
-    
-    // return 0;
+    const std::vector<int> expected{1, 3, 4, 2, 1};
+    EXPECT_EQ(expected, actual);
 }
