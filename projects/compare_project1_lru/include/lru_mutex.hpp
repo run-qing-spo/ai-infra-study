9c9efe574bc0fc9f98be7e4bf2b2cda242785a22
runqing-spo <13219070380@163.com>
2026-06-20 15:54:46 +0000

    feat(compared_lru_mutex):显式public可访问，std::move处理wrap

--- projects/compare_project1_lru/include/lru_mutex.hpp
+++ projects/compare_project1_lru/include/lru_mutex.hpp
@@ -2,25 +2,32 @@
 #include "lru_base.hpp"
 #include <cstddef> // 这个头提供std::size_t
 #include <mutex>
+#include <vector>
 
 namespace lru_mutex {
     template<typename K, typename V>
     class lrucache_mutex{
-        explicit lrucache_mutex(std::size_t capacity_):lru_base_(capacity_){}
-        void push(const K& key, std::shared_ptr<V> value) {
-            std::lock_guard<std::mutex> g(mu);
-            lru_base_.push(key, value);
-        }
+        public:
+            explicit lrucache_mutex(std::size_t capacity_):lru_base_(capacity_){}
+            void push(const K& key, std::shared_ptr<V> value) {
+                std::lock_guard<std::mutex> g(mu);
+                lru_base_.push(key, std::move(value));
+            }
 
-        std::shared_ptr<V> get(const K& key) {
-            std::lock_guard<std::mutex> g(mu);
-            return lru_base_.get(key);
-        }
+            std::shared_ptr<V> get(const K& key) {
+                std::lock_guard<std::mutex> g(mu);
+                return lru_base_.get(key);
+            }
 
-        void erase(const K& key) {
-            std::lock_guard<std::mutex> g(mu);
-            lru_base_.erase(key);
-        }
+            void erase(const K& key) {
+                std::lock_guard<std::mutex> g(mu);
+                lru_base_.erase(key);
+            }
+
+            std::vector<std::shared_ptr<V>> values() {
+                std::lock_guard<std::mutex> g(mu);
+                return lru_base_.values();
+            }
 
         private:
             mutable std::mutex mu;
